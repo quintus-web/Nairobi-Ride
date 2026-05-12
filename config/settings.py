5c1d3ef2995087302bfb297c6e17cdab2557818b
quintus-web <ongangaquintus@gmail.com>
2026-05-12 11:55:04 +0300

    Fix gunicorn binding and CSRF trusted origins for Railway

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-7lr$y^h$i%yd6g
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://' + h for h in ALLOWED_HOSTS if h not in ('127.0.0.1', 'localhost')]
 
 
 # Application definition
